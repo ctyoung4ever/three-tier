8b581f95c55fd4b91fc6bda9422adcf1a9f25f80
Collin Young <ctyoung1@byu.edu>
2022-10-27 09:04:42 -0600

    live site

--- datagrid.js
+++ datagrid.js
@@ -1,6 +1,6 @@
 // URL to your production code when you publish yoru google app script as a web app
 // You will get this URL when you publish your google apps script as a web app
-let prefix = "https://script.google.com/macros/s/AKfycbyg1zKTfVrBUdsADoWiHYkPIylzglgTqPb29vEY7C4WL5wA-9kIfSPK13LBuKAEaser/exec" // looks like: https://script.google.com/macros/s/AKfycbz90JgtbAjrjz8c8WQGjgy8kM2GtdCAulom8XWOQyGoeX8DXM5XsmUMAcSYSSyZwwNSLq/exec
+let prefix = "https://ctyoung4ever.github.io/three-tier/datagrid.html?Employee" // looks like: https://script.google.com/macros/s/AKfycbz90JgtbAjrjz8c8WQGjgy8kM2GtdCAulom8XWOQyGoeX8DXM5XsmUMAcSYSSyZwwNSLq/exec
 const dealy_seconds = 4 // how long data validation messages are visible
 
 //example call: file:///C:/Users/Gove/three-tier/datagrid.html?employee
